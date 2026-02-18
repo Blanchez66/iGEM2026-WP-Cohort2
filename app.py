6e5355b0d27d0e16bb021085868aa207f1564fcc
Blanchez66 <1807699931@qq.com>
2026-02-19 00:42:39 +0800

    create all the components without any decorate

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from os import path
+import os
 from pathlib import Path
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 template_folder = path.abspath('./wiki')
 
@@ -13,8 +14,19 @@ def home():
 
 @app.route('/<page>/')
 def pages(page):
-    print(page.lower())
-    return render_template('pages/' + page.lower() + '.html')
+    page_name = page.lower() + '.html'
+    # We are looking for the page in the 'pages' subdirectory of the template folder
+    search_dir = os.path.join(app.template_folder, 'pages')
+    for root, dirs, files in os.walk(search_dir):
+        if page_name in files:
+            # Construct the path relative to the template folder for render_template
+            full_path = os.path.join(root, page_name)
+            template_path = os.path.relpath(full_path, app.template_folder)
+            # Ensure forward slashes for the template path
+            template_path = template_path.replace('\\', '/')
+            return render_template(template_path)
+    # If the page is not found, return a 404 error
+    abort(404)
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
